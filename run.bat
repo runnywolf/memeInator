85682b2890aee088ec6bfdc3f3854dbed1f7d255
runnywolf <flowynnur@gmail.com>
2023-04-29 19:44:14 +0800

    [v0.dev-2]

--- run.bat
+++ run.bat
@@ -1,4 +1,5 @@
 @echo off
-javac -d . -encoding utf-8 src/main/*.java
 javac -d . -encoding utf-8 src/*.java
+javac -d . -encoding utf-8 src/main/*.java
+javac -d . -encoding utf-8 src/main/page/*.java
 java src.cls.MemeInatorTest
---
+++ src/main/page/StartPage.java
@@ -0,0 +1,7 @@
+package src.cls;
+
+public class StartPage{
+  public StartPage(){
+    
+  }
+}
