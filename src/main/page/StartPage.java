2ba425835531cf2b5de17b1d259dacf44ca53233
runnywolf <flowynnur@gmail.com>
2023-05-02 16:58:49 +0800

    test

--- src/main/page/StartPage.java
+++ src/main/page/StartPage.java
@@ -3,7 +3,6 @@ package main.page;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.GridLayout;
-import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
