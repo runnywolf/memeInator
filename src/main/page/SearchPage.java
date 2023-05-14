6e6c3713e94907f14fcf97ecdf2dad3bf84c3684
R1Vix <ericcfan@yahoo.com>
2023-05-14 17:50:09 +0800

    Change SearchPage
    Still WIP

--- src/main/page/SearchPage.java
+++ src/main/page/SearchPage.java
@@ -1,108 +1,83 @@
 package main.page;
 
+import java.util.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.event.*;
+import javax.swing.plaf.RootPaneUI;
+import javax.swing.plaf.basic.BasicComboBoxUI.KeyHandler;
+import javax.swing.plaf.basic.BasicTabbedPaneUI.MouseHandler;
 
 import main.MemeInator;
 import main.external.NewFont;
 
 public class SearchPage extends Page{
+
+    DefaultListModel defaultListModel = new DefaultListModel<>();
+
     public SearchPage(MemeInator frame){
         super(frame);
 
-        add(PromptInput(), Integer.valueOf(0));
-        add(SearchButton(), Integer.valueOf(0));
-        add(ScrollPane(), Integer.valueOf(0));
-        add(IndexButton1(), Integer.valueOf(0));
-        add(IndexButton2(), Integer.valueOf(0));
-        add(IndexButton3(), Integer.valueOf(0));
-        add(IndexButton4(), Integer.valueOf(0));
-        add(IndexButton5(), Integer.valueOf(0));
+        JLabel Label = new JLabel();
+        Label.setText("請輸入關鍵字：");
+        Label.setBounds(60,30,100,50);
+        add(Label);
 
-        ButtonHandler handler = new ButtonHandler();
-        SearchButton().addActionListener(handler);
-        IndexButton1().addActionListener(handler);
-        IndexButton2().addActionListener(handler);
-        IndexButton3().addActionListener(handler);
-        IndexButton4().addActionListener(handler);
-        IndexButton5().addActionListener(handler);
+        JTextArea PromptInput = new JTextArea();
+        PromptInput.setBounds(160,30,300,50);
+        add(PromptInput);
+        PromptInput.add(new JScrollPane(PromptInput));
+
+        JTable Results = new JTable();
+        Results.setFont(new Font("Serif", Font.PLAIN, 24));
+        Results.setBounds(60,100,400,500);
+        add(Results);
+
+        MouseHandler mouseHandler = new MouseHandler();
+        Results.addMouseListener(mouseHandler);
+
+        KeyHandler keyHandler = new KeyHandler();
+        PromptInput.addKeyListener(keyHandler);
+
+        this.binddata();
     }
 
-    private JTextField PromptInput(){
-        JTextField PromptLabel = new JTextField();
-        PromptLabel.setToolTipText("請輸入關鍵字：");
-        PromptLabel.setBounds(60,30,300,50);
-        return PromptLabel;
+    private ArrayList GetLabel() {
+        ArrayList Tags = new ArrayList();
+        Tags.add("標籤一");
+        Tags.add("標籤二");
+
+        return Tags;
     }
 
-    private JButton SearchButton(){
-        JButton SearchButton = new JButton();
-        SearchButton.setText("搜尋");
-        SearchButton.setBounds(360,30,100,50);
-        return SearchButton;
+    private void binddata() {
+        GetLabel().stream().forEach((Tags) -> {
+            defaultListModel.addElement(Tags);
+        }); 
+        //Results.setModel(defaultListModel);
     }
 
-    private JScrollPane ScrollPane(){
-        JScrollPane ScrollPane = new JScrollPane();
-        String[] tableColumn = {"搜尋結果"};
-        String[][] tableData = {};
-        JTable TagSearchResult = new JTable(tableData, tableColumn);
-        TagSearchResult.setFont(new Font("Serif", Font.PLAIN, 24));
-
-        
-        ScrollPane.setBounds(60,100,400,500);
-        ScrollPane.setViewportView(TagSearchResult);
-        ScrollPane.setVerticalScrollBarPolicy( JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED); 
-        return ScrollPane;
-    }
-
-    private JButton IndexButton1(){
-        JButton IndexButton1 = new JButton();
-        IndexButton1.setText("IndexButton1");
-        IndexButton1.setVisible(true);
-        return IndexButton1;
-    }
-
-    private JButton IndexButton2(){
-        JButton IndexButton2 = new JButton();
-        IndexButton2.setText("IndexButton2");
-        IndexButton2.setBounds(60,170,400,50);
-        IndexButton2.setVisible(true);
-        return IndexButton2;
-    }
-
-    private JButton IndexButton3(){
-        JButton IndexButton3 = new JButton();
-        IndexButton3.setText("IndexButton3");
-        IndexButton3.setBounds(60,240,400,50);
-        IndexButton3.setVisible(true);
-        return IndexButton3;
-    }
-
-    private JButton IndexButton4(){
-        JButton IndexButton4 = new JButton();
-        IndexButton4.setText("IndexButton4");
-        IndexButton4.setBounds(60,310,400,50);
-        IndexButton4.setVisible(true);
-        return IndexButton4;
-    }
-
-    private JButton IndexButton5(){
-        JButton IndexButton5 = new JButton();
-        IndexButton5.setText("IndexButton5");
-        IndexButton5.setBounds(60,380,400,50);
-        IndexButton5.setVisible(true);
-        return IndexButton5;
-    }
-
-    private class ButtonHandler implements ActionListener{
-        @Override
-        public void actionPerformed(ActionEvent Event) {
-            if(Event.getActionCommand().equals("搜尋")){
-                String String = "";
-                String NewString = PromptInput().getText();
+    private void searchTags(String searchTerm) {
+        DefaultListModel foundTags = new DefaultListModel();
+        ArrayList Tags = GetLabel();
+        Tags.stream().forEach((tag) -> {
+            String tagName = Tags.toString().toLowerCase();
+            if(tagName.contains(searchTerm.toLowerCase())){
+                foundTags.addElement(Tags);
             }
+        });
+        defaultListModel  = foundTags;
+        //Results.setmodel(defaultListModel);
+    }
+
+    private class MouseHandler extends MouseAdapter {
+    }
+
+    private class KeyHandler extends KeyAdapter {
+        @Override
+        public void keyReleased(KeyEvent e) {
+            searchTags(PromptInput.getText());
         }
     }
 }
