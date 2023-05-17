84aa9d25065e39e1b32a89f57894a6afa32dd737
runnywolf <flowynnur@gmail.com>
2023-05-17 22:08:47 +0800

    [v0.dev-33] update SearchPage
    add homeButton (SearchPage)
    add selectTagList

--- src/main/page/SearchPage.java
+++ src/main/page/SearchPage.java
@@ -20,40 +20,54 @@ import javax.swing.event.DocumentListener;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 import main.MemeInator;
 
 public class SearchPage extends Page{
-	private ArrayList<String[]> templateTag = new ArrayList<>();
-	private Set<String> tagSet = new HashSet<>();
+	private ArrayList<String[]> templateTag;
+	private Set<String> tagSet;
 	private JTextField searchTextField;
+	private final int MAX_SEARCH_RESULT_N = 10;
 	private JPanel searchTagList;
-	private Set<String> selectedTagSet = new HashSet<>();
+	private Set<String> selectedTagSet;
+	private final int MAX_SELECT_TAG_N = 5;
 	private JPanel selectedTag;
 
 	public SearchPage(MemeInator frame){
 		super(frame);
+		templateTag = new ArrayList<>();
+		tagSet = new HashSet<>();
+		selectedTagSet = new HashSet<>();
 
 		readTagFile();
 
+		add(makeBackButton(), Integer.valueOf(0));
 		add(makeSearchBar(), Integer.valueOf(0));
+		clearSearch();
 	}
 
-	private void readTagFile(){
-		try{
-			BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("data/template/templateTag.txt"), "UTF-8"));
+	private JPanel makeBackButton(){
+		JPanel backButtonPanel = new JPanel();
+		backButtonPanel.setOpaque(false);
+		backButtonPanel.setLayout(null);
+		backButtonPanel.setBounds(0, 0, 200, 80);
 
-			String s;
-			while ((s = reader.readLine()) != null){
-				if (!s.equals("")){
-					String[] sSplit = s.split(" ");
-					templateTag.add(sSplit);
-					for (int i = 1; i < sSplit.length; i++) tagSet.add(sSplit[i]);
-				}
+		BetterButton homeButton = new BetterButton("", 0, null, null, 0, null);
+		homeButton.setBounds(10, 10, 40, 40);
+    homeButton.whenHover(null, null, new Color(90, 90, 90), "返回主選單");
+    homeButton.setBgImage("img/toolbarIcon/home.png");
+		homeButton.whenClickGoto("StartPage");
+		homeButton.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e){
+				clearSearch();
+				removeAllTag();
 			}
+		});
+		backButtonPanel.add(homeButton);
 
-			reader.close();
-		}catch (Exception e){}
+		return backButtonPanel;
 	}
 
 	private JPanel makeSearchBar(){
@@ -74,13 +88,17 @@ public class SearchPage extends Page{
 		searchBar.setLayout(null);
 
 		searchTextField = new JTextField();
-		searchTextField.setBounds(20, 20, 200, 30);
+		searchTextField.setBounds(20, 60, 200, 30);
 		searchTextField.getDocument().addDocumentListener(new DocumentListener(){
 			public void changedUpdate(DocumentEvent e){whenInputChange();}
 			public void insertUpdate(DocumentEvent e){whenInputChange();}
 			public void removeUpdate(DocumentEvent e){whenInputChange();}
 		});
 		searchBar.add(searchTextField);
+
+		BetterLabel title = new BetterLabel("選擇的tag:", 20, null, false, SwingConstants.LEFT, null);
+		title.setBounds(20, 250, 100, 300);
+		searchBar.add(title);
 
 		return searchBar;
 	}
@@ -101,11 +119,10 @@ public class SearchPage extends Page{
 		if (searchTagList != null) remove(searchTagList);
 
 		final int TAG_BUTTON_HEIGHT = 24;
-		final int MAX_SEARCH_RESULT_N = 10;
 		int l = tagList.size();
 
 		JPanel list = new JPanel(new GridLayout(l, 1));
-		list.setBounds(20, 54, 200, TAG_BUTTON_HEIGHT*l);
+		list.setBounds(20, 94, 200, TAG_BUTTON_HEIGHT*l);
 		list.setOpaque(false);
 
 		int searchResultCount = 0;
@@ -129,23 +146,71 @@ public class SearchPage extends Page{
 		add(searchTagList, Integer.valueOf(0));
 		repaint();
 	}
+	private void clearSearch(){
+		searchTextField.setText("");
+		redrawSearchTagList(new ArrayList<>());
+	}
 	private void clickSearchTagList(String tag){
-		selectedTagSet.add(tag);
+		addTag(tag);
 		redrawSelectedTagList();
+	}
+
+	private void addTag(String tag){
+		if (selectedTagSet.size() < MAX_SELECT_TAG_N) selectedTagSet.add(tag);
+	}
+	private void removeTag(String tag){
+		selectedTagSet.remove(tag);
+	}
+	private void removeAllTag(){
+		selectedTagSet = new HashSet<>();
 	}
 
 	private void redrawSelectedTagList(){
 		if (selectedTag != null) remove(selectedTag);
 
-		JPanel tags = new JPanel();
+		int l = selectedTagSet.size();
+
+		JPanel tags = new JPanel(new GridLayout(l, 1));
 		tags.setOpaque(false);
+		tags.setBounds(20, 420, 200, 24*l);
+
+		for (String tag: selectedTagSet){
+			BetterButton button = new BetterButton(tag, 16, appBgColor, appLightColor1, 0, null);
+			button.whenHover(null, appLightColor1, new Color(120, 120, 120), null);
+			button.setHorizontalAlignment(JButton.LEFT);
+			button.addActionListener(new ActionListener(){
+				@Override
+				public void actionPerformed(ActionEvent e){
+					clickTagButton(tag);
+				}
+			});
+			tags.add(button);
+		}
 
 		selectedTag = tags;
 		add(selectedTag, Integer.valueOf(0));
+		repaint();
 	}
-	private void clickDeleteButton(){
-		// delete tag
+	private void clickTagButton(String tag){
+		removeTag(tag);
 		redrawSelectedTagList();
+	}
+
+	private void readTagFile(){
+		try{
+			BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("data/template/templateTag.txt"), "UTF-8"));
+
+			String s;
+			while ((s = reader.readLine()) != null){
+				if (!s.equals("")){
+					String[] sSplit = s.split(" ");
+					templateTag.add(sSplit);
+					for (int i = 1; i < sSplit.length; i++) tagSet.add(sSplit[i]);
+				}
+			}
+
+			reader.close();
+		}catch (Exception e){}
 	}
 
 	private int LCS(String a, String b){
